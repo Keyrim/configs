45681ef514d537d99170b4506dd01faae75e9c33
Théo Magne <theo.magne.fr@gmail.com>
2023-10-05 19:17:18 +0200

    add cuda ros and gazebo garden install script

--- usbipd/install-usbip.sh
+++ usbipd/install-usbip.sh
@@ -3,7 +3,6 @@
 # Mettre à jour la liste des packages
 sudo apt update
 
-
 # Installer les packages
 sudo apt install linux-tools-virtual hwdata
 sudo update-alternatives --install /usr/local/bin/usbip usbip `ls /usr/lib/linux-tools/*/usbip | tail -n1` 20
